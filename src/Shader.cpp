2debe5cf679e8074633f8e0c0dcca6cb267e62d4
jrebort <jrebort@outlook.com>
2023-08-28 00:33:42 +0800

    shader

---
+++ src/Shader.cpp
@@ -0,0 +1,31 @@
+#include "Shader.h"
+
+Shader::Shader(const std::string& filepath)
+{
+	
+}
+
+Shader::~Shader()
+{
+
+}
+
+void Shader::Bind() const
+{
+
+}
+
+void Shader::Unbind() const
+{
+
+}
+
+const std::string& Shader::SetUniform4f()
+{
+
+}
+
+unsigned int Shader::GetUniformLocation(const std::string& name)
+{
+
+}
